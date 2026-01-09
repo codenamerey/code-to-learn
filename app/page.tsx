539b3a62db0f6a92f8c921e2d905213312090679
Rey <100455152+codenamerey@users.noreply.github.com>
2026-01-09 14:57:44 +0800

    refactor: place 'code to learn' in header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-[overpass] bg-zinc-50 dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-col min-h-screen items-center justify-center font-[overpass] bg-zinc-50 dark:bg-black">
+      <header className="p-8">
         <h1 className="text-5xl font-bold text-zinc-900 dark:text-white sm:text-6xl">
           Code To Learn
         </h1>
-      </main>
+      </header>
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
     </div>
   );
 }
