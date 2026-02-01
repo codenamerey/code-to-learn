a126cfd5fc1912814a9d864683d29870158ee723
cerastvs <yuriksoliman@gmail.com>
2026-02-01 15:10:26 +0800

    feat: adds navigation bar

---
+++ components/NavBar.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { ChevronDown } from "lucide-react"
+import Image from "next/image"
+export default function({title}: {title: string}){
+    return <div className="flex h-[8vh] border items-center justify-between p-4">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <div className="flex items-center gap-2">
+            <ChevronDown />
+            <Image src="globe.svg" alt="user profile" width={50} height={50}></Image>
+        </div>
+    </div>
+}
