c7f5a8e4121a4b95dcecaa6a6301be3621c56ce8
cerastvs <yuriksoliman@gmail.com>
2026-02-03 17:48:10 +0800

    style: improve style and add lesson rendering

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,47 @@
 "use client";
 import NavBar from "@/components/NavBar";
-import { Card, CardTitle } from "@/components/ui/card";
+import { Card, CardTitle, CardDescription } from "@/components/ui/card";
 import { categories } from "@/lib/categories/cetegories";
-import { Radical } from "lucide-react";
+import { Brain, Radical } from "lucide-react";
 import { useState } from "react";
 import { Scrollbars } from "react-custom-scrollbars";
 
 export default function Dashboard() {
+  const renderLessons = (categoryId: number) => {
+    return categories[categoryId].lessons.map((lesson) => (
+      <Card className="w-full border border-black p-4">
+        <div className="flex gap-1 items-center">
+          <CardTitle className="text-xl font-bold">{lesson.title}</CardTitle>{" "}
+          <h2 className="text-[#0995BC] text-sm">{lesson.author}</h2>
+        </div>
+        <CardDescription>{lesson.description}</CardDescription>
+        <div className="flex gap-2 items-center">
+          <Brain className="opacity-[70%]" />
+          <CardDescription>{lesson.learnCount} Learned</CardDescription>
+        </div>
+      </Card>
+    ));
+  };
   const [activeCategory, setActiveCategory] = useState(1);
   const handleCardClick = (cardId: number) => {
+    console.log(cardId);
     setActiveCategory(cardId);
   };
   return (
-    <section className="flex flex-col items-center bg-[#EAEAEA]">
+    <section className="flex flex-col items-center bg-[#EAEAEA] ">
       <NavBar title="Dashboard" />
-      <section className=" w-[47%] border h-[120vh] pl-[24px] pt-[48px] bg-white rounded-lg">
+      <section className=" w-[47%] border h-auto p-6 bg-white rounded-lg">
         <h2 className="text-2xl pb-[16px] font-bold">Categories</h2>
         <Scrollbars autoHeight autoHeightMax={600} autoHide>
-          <div className="flex gap-4 w-full p-4 overflow-x-auto">
+          <div className="flex gap-4 w-full p-4 overflow-x-auto ">
             {categories.map((category, index) => (
               <Card
                 key={category.id ?? index}
                 onClick={() => handleCardClick(category.id)}
-                className="flex flex-col items-center justify-between p-2 cursor-pointer transition-all duration-300 transform h-52"
+                className="flex flex-col items-center justify-between p-2 cursor-pointer transition-all duration-300 transform h-38"
               >
                 <Radical
-                  className={`h-32 w-32 border rounded-lg transition-all duration-300 hover:-translate-y-1 hover:scale-105 hover:shadow-lg  ${
+                  className={`h-22 w-22 border rounded-lg transition-all duration-500 hover:-translate-y-1 hover:scale-105 hover:shadow-lg  ${
                     activeCategory === category.id
                       ? "ring-2 ring-blue-500 shadow-[0_0_20px_rgba(59,130,246,0.6)]"
                       : "shadow-md"
@@ -47,22 +63,19 @@ export default function Dashboard() {
           </div>
         </Scrollbars>
 
-        <section className="pt-6">
-          <div className="flex gap-2 items-center">
+        <section className="pt-6 w-full">
+          <div className="flex gap-2 items-center mb-4">
             <h2 className="text-2xl font-bold">Courses</h2>
-            <input type="text" className="border-2 rounded-md w-[295px]" />
-
-            <select id="fruits" name="fruits">
-              <option value="apple">Apple</option>
-              <option value="banana">Banana</option>
-              <option value="cherry">Cherry</option>
-              <option value="orange" selected>
-                Orange
-              </option>
-            </select>
-            <input type="checkbox" />
-            <h3 className="">Asc</h3>
+            <input
+              type="text"
+              className="border border-black rounded-md w-[295px] bg-[rgba(0,0,0,8%)]"
+              placeholder="Find lessons..."
+            />
           </div>
+
+          <section className="flex flex-col gap-2">
+            {renderLessons(activeCategory - 1)}
+          </section>
         </section>
       </section>
     </section>
