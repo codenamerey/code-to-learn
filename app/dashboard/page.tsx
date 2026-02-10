bc23f4e58cccbc274e341ba229fca356fc1898c1
Rey <100455152+codenamerey@users.noreply.github.com>
2026-02-10 09:18:00 +0800

    style: dashboard adapts to content

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -16,26 +16,25 @@ export default function Dashboard() {
 
   return (
     <ActiveCategoryContext.Provider value={activeCategory}>
-      <section className="h-screen flex flex-col items-center bg-[#EAEAEA] ">
-        <NavBar title="Dashboard" />
-
-        <section className="w-[47%] border h-auto p-6 bg-white rounded-lg">
-          <CategoriesSection onCardClick={handleCardClick} />
-
-          <section className="pt-6 w-full">
-            <div className="flex gap-2 items-center mb-4">
-              <h2 className="text-2xl font-bold">Courses</h2>
-              <input
-                type="text"
-                className="border border-black rounded-md w-[295px] bg-[rgba(0,0,0,8%)] pl-3"
-                placeholder="Find lessons..."
-              />
-            </div>
-
-            <CoursesSection />
+      <main className="p-4 bg-[#EAEAEA]">
+        <section className="min-h-screen flex flex-col items-center bg-[#EAEAEA] ">
+          <NavBar title="Dashboard" />
+          <section className="w-[47%] border h-auto p-6 bg-white rounded-lg">
+            <CategoriesSection onCardClick={handleCardClick} />
+            <section className="pt-6 w-full">
+              <div className="flex gap-2 items-center mb-4">
+                <h2 className="text-2xl font-bold">Courses</h2>
+                <input
+                  type="text"
+                  className="border border-black rounded-md w-[295px] bg-[rgba(0,0,0,8%)] pl-3"
+                  placeholder="Find lessons..."
+                />
+              </div>
+              <CoursesSection />
+            </section>
           </section>
         </section>
-      </section>
+      </main>
     </ActiveCategoryContext.Provider>
   );
 }
